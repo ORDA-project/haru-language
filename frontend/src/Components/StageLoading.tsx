dafe2c0d8750fabf2b63df13257b3dc6c8adafd5
syc031023 <syc031023@naver.com>
2025-01-04 01:00:43 +0900

    Question css 수정

---
+++ frontend/src/Components/StageLoading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Stage, Span } from "../Styles/Question";
+import { Spinner } from "basic-loading";
+
+const StageLoading = () => (
+  <Stage>
+    <Span>예문을 만들고 있어요.</Span>
+    <Span>잠시 기다려주세요.</Span>
+    <Spinner option={{size: 50, bgColor: "#00daaa", barColor: "rgba(0, 218, 171, 0.44)"}} />
+  </Stage>
+);
+
+export default StageLoading;
