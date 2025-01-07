943c7ffe5f19a9fe5b4b5ac403407fcd42a1532a
syc031023 <syc031023@naver.com>
2025-01-07 17:30:13 +0900

    Home css

--- frontend/src/Components/Pages/Home.tsx
+++ frontend/src/Components/Pages/Home.tsx
@@ -1,27 +1,44 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import HomeInfoComponent from "../Elements/HomeInfo"; // 기존 HomeInfo를 가져옵니다.
+import HomeInfo from "../Elements/HomeInfo"; // 기존 HomeInfo를 가져옵니다.
 import NavBar from "../Templates/Navbar";
+import HomeHeader from "../Elements/HomeHeader";
+// import Login from "../Pages/Login";
 
-interface HomeProps {}
+interface HomeProps {
+    Login: boolean;
+}
 
-const Home = (props: HomeProps) => {
+const Home = ({Login = true}: HomeProps) => {
     const [isLogin, setLogin] = useState<boolean>(true);
-    
-
 
     useEffect(() => {
-        setLogin(true);
+        setLogin(Login);
         
     }, []);
 
     return (
-        <>
-            <HomeInfoComponent isLogin={isLogin} userName={"진희"} visitCount={10} />
-            <NavBar />
-        </>
+        <HomeContainer>
+            {isLogin ? (
+                <HomeDiv>
+                    <HomeHeader />
+                    <HomeInfo userName={"진희"} visitCount={10} mostVisitedDay={"월요일"} recommendation={"Santa Tell Me by Ariana Grande"}/>
+                </HomeDiv>
+            ) : (
+                <div>로그인 후 이용 가능</div>
+            )}
+            <NavBar currentPage={"Home"}/>
+        </HomeContainer>
     );
 };
 
 export default Home;
 
+const HomeContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+`;
+
+const HomeDiv = styled.div`
+    padding: 5vw;
+`;
