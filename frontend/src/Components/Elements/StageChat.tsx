968b57e7edf804aa6cdb4a2870e5b901f98e7f73
gyry012 <cickle878@gmail.com>
2025-12-25 04:30:59 +0900

    fix: 빌드에러 수정

--- frontend/src/Components/Elements/StageChat.tsx
+++ frontend/src/Components/Elements/StageChat.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { useAtom } from "jotai";
 import { isLargeTextModeAtom } from "../../store/dataStore";
 import axios from "axios";
